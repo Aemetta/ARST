b4f73b3390149be4a9df16ca34266f8014999031
Æmetta <darerd@gmail.com>
2017-10-11 09:55:59 -0700

    Menus with many items now scroll

--- core/src/com/aemetta/arst/display/MenuDisplay.java
+++ core/src/com/aemetta/arst/display/MenuDisplay.java
@@ -18,6 +18,10 @@ public class MenuDisplay implements Display {
 	
 	private String path;
 	
+	private int scroll;
+	private int scope;
+	private int maxScope = 7;
+	
 	public MenuDisplay(String p, MenuSelector ms, AssetManager manager) {
 		this.ms = ms;
 		this.path = "Menus/"+p+"/";
@@ -25,6 +29,9 @@ public class MenuDisplay implements Display {
 		manager.load(path + "logo.png", Texture.class);
 		manager.load(path + "/fonts/normal.fnt", BitmapFont.class);
 		manager.load(path + "/fonts/selected.fnt", BitmapFont.class);
+		
+		this.scroll = 0;
+		this.scope = 0;
 	}
 	
 	public void init(AssetManager manager) {
@@ -38,26 +45,34 @@ public class MenuDisplay implements Display {
 		
 		if(ms.isMainMenu()) batch.draw(logo, -logo.getWidth()/2, 100);
 		
-		for(int i = 0; i < ms.numberOfItems(); i++) {
-			if(i==ms.getSelection() && ms.isActivated()) continue;
+		scope = (ms.numberOfItems() < maxScope) ? ms.numberOfItems() : maxScope;
+		
+		int sel = ms.getSelection();
+		if(sel < scroll) scroll = sel;
+		else if(sel >= scroll + scope) scroll = sel - scope + 1;
+		
+		if(scroll + scope > ms.numberOfItems()) scroll = 0;
+		
+		for(int i = scroll; i < scope+scroll; i++) {
+			if(i==sel && ms.isActivated()) continue;
 			drawText(batch, font, i, ms.isMainMenu(), ms.isSetting(i));
 		}
 		if(ms.isActivated())
-			drawText(batch, select, ms.getSelection(),
-					ms.isMainMenu(), ms.isSetting(ms.getSelection()));
+			drawText(batch, select, sel,
+					ms.isMainMenu(), ms.isSetting(sel));
 	}
 	
 	private void drawText(Batch batch, BitmapFont f, int i, boolean main, boolean setting){
 		if(setting) {
 			f.draw(batch, ms.getItems()[i].toLowerCase(),
-				-250, -50*((main) ? i : i-ms.numberOfItems()/2),
+				-250, -50*((main) ? i-scroll : i-scroll-scope/2),
 				500, -1, false);
 			f.draw(batch, ms.getSetting(i).toLowerCase(),
-					-250, -50*((main) ? i : i-ms.numberOfItems()/2),
+					-250, -50*((main) ? i-scroll : i-scroll-scope/2),
 					500, 2, false);
 		} else {
 			f.draw(batch, ms.getItems()[i].toLowerCase(),
-					-100, -50*((main) ? i : i-ms.numberOfItems()/2),
+					-100, -50*((main) ? i-scroll : i-scroll-scope/2),
 					200, 1, false);
 		}
 	}
